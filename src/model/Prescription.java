05b79bd8d8fa99560dad8ab05becbcdd3636b572
Kira060200 <constantin.gab02@gmail.com>
2021-03-11 16:48:26 +0200

    Prescription

--- src/model/Prescription.java
+++ src/model/Prescription.java
@@ -1,4 +1,27 @@
 package model;
 
 public class Prescription {
+    private long id;
+    private Drug[] drug;
+
+    public Prescription(long id, Drug[] drug) {
+        this.id = id;
+        this.drug = drug;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Drug[] getDrug() {
+        return drug;
+    }
+
+    public void setDrug(Drug[] drug) {
+        this.drug = drug;
+    }
 }
