34002ac755ee7a5082f8e67f54410607fe83516c
Kira060200 <constantin.gab02@gmail.com>
2021-03-11 16:56:10 +0200

    Surgery

--- src/model/MedicalSurgery.java
+++ src/model/MedicalSurgery.java
@@ -1,4 +1,28 @@
 package model;
 
-public class MedicalSurgery {
+public class MedicalSurgery extends Appointment{
+    private String type;
+    private Assistant as;
+
+    public MedicalSurgery(long id, String date, float price, Doctor doc, Patient pat, String type, Assistant as) {
+        super(id, date, price, doc, pat);
+        this.type = type;
+        this.as = as;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Assistant getAs() {
+        return as;
+    }
+
+    public void setAs(Assistant as) {
+        this.as = as;
+    }
 }
