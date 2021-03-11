7bcb9e914193988ee77b143439f300240c115d42
Kira060200 <constantin.gab02@gmail.com>
2021-03-11 16:40:33 +0200

    Drug

--- src/model/Drug.java
+++ src/model/Drug.java
@@ -1,4 +1,37 @@
 package model;
 
 public class Drug {
+    private long id;
+    private String name;
+    private float price;
+
+    public Drug(long id, String name, float price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }
