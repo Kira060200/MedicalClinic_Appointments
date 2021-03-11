36d93fd356e75038962b81c32d9527138e1d7ce5
Kira060200 <constantin.gab02@gmail.com>
2021-03-11 16:37:18 +0200

    Assistant

--- src/model/Assistant.java
+++ src/model/Assistant.java
@@ -1,4 +1,18 @@
 package model;
 
-public class Assistant {
+public class Assistant extends MedicalStaff{
+    private Doctor doc;
+
+    public Assistant(long id, String firstName, String lastName, int age, String sex, String phoneNumber, float salary, int experience, Doctor doc) {
+        super(id, firstName, lastName, age, sex, phoneNumber, salary, experience);
+        this.doc = doc;
+    }
+
+    public Doctor getDoc() {
+        return doc;
+    }
+
+    public void setDoc(Doctor doc) {
+        this.doc = doc;
+    }
 }
