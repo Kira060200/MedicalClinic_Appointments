09f3329978425a400ab5b36fd87d9adf1dc43ee8
Kira060200 <constantin.gab02@gmail.com>
2021-03-11 16:26:17 +0200

    Person

--- src/model/Person.java
+++ src/model/Person.java
@@ -1,4 +1,67 @@
 package model;
 
-public class Person {
+public abstract class Person {
+    protected long id;
+    protected String firstName;
+    protected String lastName;
+    protected int age;
+    protected String sex;
+    protected String phoneNumber;
+
+    public Person(long id, String firstName, String lastName, int age, String sex, String phoneNumber) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.sex = sex;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
