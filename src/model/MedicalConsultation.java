b7a4e2afaff84cb8f75444d1dab738ef68c91b6a
Kira060200 <constantin.gab02@gmail.com>
2021-03-11 16:54:04 +0200

    Consultation

--- src/model/MedicalConsultation.java
+++ src/model/MedicalConsultation.java
@@ -1,4 +1,28 @@
 package model;
 
-public class MedicalConsultation {
+public class MedicalConsultation extends Appointment{
+    private String disease;
+    private Prescription pr;
+
+    public MedicalConsultation(long id, String date, float price, Doctor doc, Patient pat, String disease, Prescription pr) {
+        super(id, date, price, doc, pat);
+        this.disease = disease;
+        this.pr = pr;
+    }
+
+    public String getDisease() {
+        return disease;
+    }
+
+    public void setDisease(String disease) {
+        this.disease = disease;
+    }
+
+    public Prescription getPr() {
+        return pr;
+    }
+
+    public void setPr(Prescription pr) {
+        this.pr = pr;
+    }
 }
