65605c964c31ae0db2e4ccef4b86f32353ead0bc
Kira060200 <constantin.gab02@gmail.com>
2021-03-11 16:58:18 +0200

    MedicalClinic

--- src/model/MedicalClinic.java
+++ src/model/MedicalClinic.java
@@ -1,4 +1,22 @@
 package model;
 
 public class MedicalClinic {
+    private MedicalStaff[] staff;
+    private Patient[] patients;
+
+    public MedicalStaff[] getStaff() {
+        return staff;
+    }
+
+    public void setStaff(MedicalStaff[] staff) {
+        this.staff = staff;
+    }
+
+    public Patient[] getPatients() {
+        return patients;
+    }
+
+    public void setPatients(Patient[] patients) {
+        this.patients = patients;
+    }
 }
