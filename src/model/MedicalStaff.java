b55cf95f3b26f116941343e4aafd57e7c37c3f8e
Kira060200 <constantin.gab02@gmail.com>
2021-03-11 16:27:48 +0200

    MedicalStaff

--- src/model/MedicalStaff.java
+++ src/model/MedicalStaff.java
@@ -1,4 +1,27 @@
 package model;
 
-public class MedicalStaff {
+public abstract class MedicalStaff {
+    protected float salary;
+    protected int experience;
+
+    public MedicalStaff(float salary, int experience) {
+        this.salary = salary;
+        this.experience = experience;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
 }
