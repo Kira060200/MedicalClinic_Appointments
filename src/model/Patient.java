23c1dc2e4bf84e676500c97f36ebb08bf3b97be1
Kira060200 <constantin.gab02@gmail.com>
2021-03-11 16:30:23 +0200

    Patient

--- src/model/Patient.java
+++ src/model/Patient.java
@@ -1,4 +1,18 @@
 package model;
 
-public class Patient {
+public class Patient extends Person{
+    private String[] disease;
+
+    public Patient(long id, String firstName, String lastName, int age, String sex, String phoneNumber, String[] disease) {
+        super(id, firstName, lastName, age, sex, phoneNumber);
+        this.disease = disease;
+    }
+
+    public String[] getDisease() {
+        return disease;
+    }
+
+    public void setDisease(String[] disease) {
+        this.disease = disease;
+    }
 }
