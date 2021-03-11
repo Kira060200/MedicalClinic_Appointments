ab0e11fdabd5ebf0f13db98ba5c402107d593f71
Kira060200 <constantin.gab02@gmail.com>
2021-03-11 17:13:58 +0200

    project schema

--- src/main/Application.java
+++ src/main/Application.java
@@ -1,4 +1,24 @@
 package main;
 
+/*
+
+main:
+    Application
+model:
+    1 MedicalClinic ([]MedicalStaff, []Patient)
+    2 abstract Appointment (id, date, price, Doctor, Patient)
+        2.1 MedicalConsultation (disease, Prescription)
+        2.2 MedicalSurgery (type, Assistant)
+    3 abstract Person (id, firstName, lastName, age, sex, phoneNumber)
+        3.1 Patient ([]disease)
+        3.2 abstract MedicalStaff (salary, experience)
+            3.2.1 Doctor (branch, []Assistant)
+            3.2.2 Assistant (boolean resident)
+    4 Drug (id, name, price)
+    5 Prescription (id, []Drug)
+service:
+    ClinicalManagement
+
+ */
 public class Application {
 }
