fd724e9bbd51b4a2a7d1410420f77c4be4f1a652
Kira060200 <constantin.gab02@gmail.com>
2021-03-11 16:36:26 +0200

    Doctor

--- src/model/Doctor.java
+++ src/model/Doctor.java
@@ -1,4 +1,28 @@
 package model;
 
-public class Doctor {
+public class Doctor extends MedicalStaff{
+    private String branch;
+    private Assistant[] assistant;
+
+    public Doctor(long id, String firstName, String lastName, int age, String sex, String phoneNumber, float salary, int experience, String branch, Assistant[] assistant) {
+        super(id, firstName, lastName, age, sex, phoneNumber, salary, experience);
+        this.branch = branch;
+        this.assistant = assistant;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public Assistant[] getAssistant() {
+        return assistant;
+    }
+
+    public void setAssistant(Assistant[] assistant) {
+        this.assistant = assistant;
+    }
 }
