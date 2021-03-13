f5676dffeafa79ad01efb848e0ba778c14e43327
Kira060200 <constantin.gab02@gmail.com>
2021-03-13 19:13:39 +0200

    basic service class

--- src/service/ClinicalManagement.java
+++ src/service/ClinicalManagement.java
@@ -1,4 +1,12 @@
 package service;
 
+import model.*;
+
 public class ClinicalManagement {
+    public void addStaff(MedicalClinic clinic, MedicalStaff staff) {
+        clinic.getStaff()[clinic.getStaff().length] = staff;
+    }
+    public void addPatient(MedicalClinic clinic, Patient patient) {
+        clinic.getPatients()[clinic.getPatients().length] = patient;
+    }
 }
