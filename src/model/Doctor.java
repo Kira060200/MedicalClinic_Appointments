8e128848283ddf040e7e3d7578ee8b217141b728
Kira060200 <constantin.gab02@gmail.com>
2021-03-13 19:13:14 +0200

    Doctor

--- src/model/Doctor.java
+++ src/model/Doctor.java
@@ -4,6 +4,11 @@ public class Doctor extends MedicalStaff{
     private String branch;
     private Assistant[] assistant;
 
+    public Doctor(long id, String firstName, String lastName, int age, String sex, String phoneNumber, float salary, int experience, String branch) {
+        super(id, firstName, lastName, age, sex, phoneNumber, salary, experience);
+        this.branch = branch;
+    }
+
     public Doctor(long id, String firstName, String lastName, int age, String sex, String phoneNumber, float salary, int experience, String branch, Assistant[] assistant) {
         super(id, firstName, lastName, age, sex, phoneNumber, salary, experience);
         this.branch = branch;
