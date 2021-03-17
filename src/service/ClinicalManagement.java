cded72506e0051dbbf02970ffbc6f7c971af4c6e
Kira060200 <constantin.gab02@gmail.com>
2021-03-17 10:28:57 +0200

    get and print for MedicalStaff and Patient

--- src/service/ClinicalManagement.java
+++ src/service/ClinicalManagement.java
@@ -4,9 +4,48 @@ import model.*;
 
 public class ClinicalManagement {
     public void addStaff(MedicalClinic clinic, MedicalStaff staff) {
-        clinic.getStaff()[clinic.getStaff().length] = staff;
+        int nextAvailableIndex = getNumberOfStaff(clinic);
+        clinic.getStaff()[nextAvailableIndex] = staff;
     }
+
     public void addPatient(MedicalClinic clinic, Patient patient) {
-        clinic.getPatients()[clinic.getPatients().length] = patient;
+        int nextAvailableIndex = getNumberOfPatients(clinic);
+        clinic.getPatients()[nextAvailableIndex] = patient;
+    }
+
+    public void printStaffDetails(MedicalClinic clinic) {
+        for(MedicalStaff p : clinic.getStaff()) {
+            if(p != null) {
+                System.out.println(p);
+            }
+        }
+    }
+
+    private int getNumberOfStaff(MedicalClinic clinic) {
+        int numberOfStaff = 0;
+        for(MedicalStaff p : clinic.getStaff()) {
+            if(p != null) {
+                numberOfStaff++;
+            }
+        }
+        return numberOfStaff;
+    }
+
+    public void printPatientsDetails(MedicalClinic clinic) {
+        for(Patient p : clinic.getPatients()) {
+            if(p != null) {
+                System.out.println(p);
+            }
+        }
+    }
+
+    private int getNumberOfPatients(MedicalClinic clinic) {
+        int numberOfPatients = 0;
+        for(Patient p : clinic.getPatients()) {
+            if(p != null) {
+                numberOfPatients++;
+            }
+        }
+        return numberOfPatients;
     }
 }
